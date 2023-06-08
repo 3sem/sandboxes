83e5f6d36ebd4569ce0ee79a47a3f4a6b469c7b7
name <31904035+3sem@users.noreply.github.com>
2023-06-08 21:43:56 +0300

    Update iter_superset.py

--- subgraph_matcher/iter_superset.py
+++ subgraph_matcher/iter_superset.py
@@ -1,6 +1,9 @@
 def it_superset(l=[1,2,3,4], lim=3)
-    space = 2 ** min(lim, len(l))
+    space = 2 ** len(l)
     pss = more_itertools.powerset(l)
     pss.__next__()
     for i in range(1, space):
-        print(pss.__next__())
+        p = pss.__next__()
+        if len(p) > lim:
+            break
+        print(p)
