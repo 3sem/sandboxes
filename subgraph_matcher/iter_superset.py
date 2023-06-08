641983740f69810d58f019520a93ea806e752f5e
name <31904035+3sem@users.noreply.github.com>
2023-06-08 21:39:14 +0300

    Create iter_superset.py

---
+++ subgraph_matcher/iter_superset.py
@@ -0,0 +1,6 @@
+def it_supersets(l=[1,2,3,4], lim=3)
+    space = 2 ** min(lim, len(l))
+    pss = more_itertools.powerset(l)
+    pss.__next__()
+    for i in range(1, space):
+        print(pss.__next__())
