3f698a5779473c7034100ea95201c4b3f1496e23
name <31904035+3sem@users.noreply.github.com>
2023-06-13 09:43:54 +0300

    Update complexity_estimations.m

--- subgraph_matcher/complexity_estimations.m
+++ subgraph_matcher/complexity_estimations.m
@@ -8,4 +8,4 @@ In 2b), there is more complex estimation...
 3) For each subgraph, the isomorphism should be checked. Let the search of already matched subgraphs requires O(1) (dict with WL hash as a key)
 Then, need to calculate WL hash for tested graph -- $\mu(t) ^ {x}$, where $x  \in [2..3]$.
 
-Thus, total complexity is $O(2 ^ \mu(t) \cdot N \cdot \mu(t) ^ x)$.
+Thus, total complexity is $O(2 ^ \mu(t) \dot N \dot \mu(t) ^ x)$.
