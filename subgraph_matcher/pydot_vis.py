2c2a5da240871394e7922efcae32e9667fa6db3b
name <31904035+3sem@users.noreply.github.com>
2023-11-06 13:13:08 +0300

    Update pydot_vis.py

--- subgraph_matcher/pydot_vis.py
+++ subgraph_matcher/pydot_vis.py
@@ -2,23 +2,56 @@ import networkx as nx
 import pydot
 from enum import Enum 
 
-palette = {
+
+border_color_palette = [
+    'aqua',
+    'azure',
+    'beige',
+    'blue',
+    'brown',
+    'charteuse',
+    'coral',
+    'crimson',
+    'darkgreen',
+    'fuchsia',
+    'grey',
+    'lavender',
+    'magenta',
+    'maroon',
+    'navy',
+    'salmon',
+    'red',
+    'wheat',
+    'yellowgreen'
+
+]
+
+
+node_classes_palette = {
     "r" : ('red', 'filled'),
     "g" : ('red' + ":"+ 'green' + ";0.25:" + 'blue', 'filled, rounded, wedged'),
     "b" : ('lightblue', 'filled')
 }
 
-def save_colored_pydot_graph(G: nx.DiGraph, nm="____ddd____.dot", num_palette=palette, label='group', visualize_by_colors=True):
+edle_classes_palette = border_color_palette
+
+
+
+
+
+def save_colored_pydot_graph(G: nx.DiGraph, nm="____ddd____.dot",
+                             node_palette=node_classes_palette, 
+                             label='group', visualize_by_colors=True,
+                             color_edges=True, edge_palette=edge_classes_palette):
     pdot = to_pydot(G)
-    #num_palette = dict()
-    #if visualize_by_colors:
-        #num_palette = prepare_colors_dict(G)
+    
 
     for i, node in enumerate(pdot.get_nodes()):
         if visualize_by_colors:
             try:
-                clr_p = num_palette[int(node.obj_dict['attributes'].get(label, 1))]
-                node.set_color('white')
+                clr_p = node_palette[int(node.obj_dict['attributes'].get(label, 1))]
+                border_color = border_color_palette[node.obj_dict['attributes'].get('border_color', 1)]
+                node.set_color(border_color)
                 node.set_fillcolor(clr_p[0]) #+";0.15:"+clrs)
                 node.set_style(clrp[1])
                 
@@ -30,8 +63,14 @@ def save_colored_pydot_graph(G: nx.DiGraph, nm="____ddd____.dot", num_palette=pa
         else:
             node.set_style('rounded, filled')
        
-        node.set_label( node.obj_dict['attributes'].get(label, None))
-       
+        node.set_label(node.obj_dict['attributes'].get(label, None))
+
+    if color_edges:
+        for i, edge in enumerate(pdot.get_edges()):
+            ek = edge.obj_dict['attributes'].get('border_color', '1')
+            edge.set_label(ek)
+            edge.set_color(edge_classes_palette[int(ek)])
+
     pdot.write_dot(nm)
     
     return
